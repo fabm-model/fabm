1518d152b132573d491f8dfd4a22d585e39e8bcf
jornbr <jornbr@5362ad47-ea5d-43ec-ade3-0ade0361cc23>
2011-02-16 16:33:28 +0000

    changed preprocessor defines for 1d
    git-svn-id: https://rmbm.svn.sourceforge.net/svnroot/rmbm@177 5362ad47-ea5d-43ec-ade3-0ade0361cc23

--- include/fabm.h
+++ include/fabm.h
@@ -278,6 +278,7 @@
 
 ! Work-in-progress: extra definitions for coupling to pure-1D models [ERSEM]
 #define _GET_NLEV_ 150
+#define _DOMAIN_1D_ 1:fabm_loop_stop-fabm_loop_start+1
 #define _GET_STATE_1D_(variable,target) target = env%var(variable%dependencyid)%data(fabm_loop_start:fabm_loop_stop)
 #define _GET_DEPENDENCY_1D_(variable,target) target = env%var(variable)%data(fabm_loop_start:fabm_loop_stop)
 #define _FABM_LOOP_BEGIN_1D_
@@ -286,5 +287,4 @@
 #define _INDEX_ODE_1D_(variable) (fabm_loop_start:fabm_loop_stop,variable)
 #endif
 #define _SET_ODE_1D_ rhs _INDEX_ODE_1D_(variable%id) = rhs _INDEX_ODE_1D_(variable%id) + (value)
-#define _SET_EXTINCTION_1D_ extinction(fabm_loop_start:fabm_loop_stop) = extinction(fabm_loop_start:fabm_loop_stop) + value(fabm_loop_start:fabm_loop_stop)
-#define _DOMAIN_1D_ fabm_loop_start:fabm_loop_stop
+#define _SET_EXTINCTION_1D_ extinction(fabm_loop_start:fabm_loop_stop) = extinction(fabm_loop_start:fabm_loop_stop) + value
