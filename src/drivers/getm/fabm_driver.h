49ea2a15d23417e2e71e14a36186ea4bb2438ecf
bolding <bolding@5362ad47-ea5d-43ec-ade3-0ade0361cc23>
2011-02-22 16:27:48 +0000

    GETM driver compiles - no 1D vetorisation
    git-svn-id: https://rmbm.svn.sourceforge.net/svnroot/rmbm@217 5362ad47-ea5d-43ec-ade3-0ade0361cc23

--- src/drivers/getm/fabm_driver.h
+++ src/drivers/getm/fabm_driver.h
@@ -1,12 +1,19 @@
 #define _FABM_DIMENSION_COUNT_ 3
-#define _FABM_HORIZONTAL_IS_SCALAR_
+!#define _FABM_HORIZONTAL_IS_SCALAR_
 
 ! Variable name and dimension specifyer for full bio fields
 #define _LOCATION_ ii,jj,kk
 #define _LOCATION_DIMENSIONS_ :,:,:
 
+! Variable name and dimension specifyer for horizontal-only bio fields
+#define _LOCATION_HZ_ ii,jj
+#define _LOCATION_DIMENSIONS_HZ_ :,:
+
+#define _VARIABLE_1DLOOP_ kk
+#define _LOCATION_1DLOOP_ ii,jj
+
 ! Let FABM manage (declare and allocate) arrays for diagnostic variables - slight performance gain.
-#define _FABM_MANAGE_DIAGNOSTICS_
+!#define _FABM_MANAGE_DIAGNOSTICS_
 
 ! Tell FABM that we want a 1D vectorized version
 !#define _FABM_USE_1D_LOOP_
@@ -16,15 +23,17 @@
 ! (FABM expects the vectorized spatial dimension to come first, which would benefit performance)
 ! Therefore, prescribe how to access spatially-localized rhs,pp,dd elements here.
 ! Note that this is only relevant if _FABM_USE_1D_LOOP_ is defined.
+#ifdef _FABM_USE_1D_LOOP_
 #define _INDEX_ODE_(i) (i,_LOCATION_-fabm_loop_start+1)
 #define _INDEX_ODE_1D_(i) (i,1:fabm_loop_stop-fabm_loop_start+1)
 #define _INDEX_PPDD_(i,j) (i,j,_LOCATION_-fabm_loop_start+1)
+#endif
 
 ! Include FABM preprocessor definitions.
 ! This *must* be done after the host-specific variables are defined (above),
 ! because these are used in fabm.h.
 #include "fabm.h"
 
-! Not used by FABM itself; only by GOTM-FABM driver gotm_fabm.F90
+! Not used by FABM itself; only by GETM-FABM driver getm_fabm.F90
 #define _LOCATION_RANGE_ 0:kk
 
