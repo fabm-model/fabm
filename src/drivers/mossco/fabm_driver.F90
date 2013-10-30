9b276a37146277373d4526697e4f436c01be1d4a
Karsten Bolding <karsten@bolding-burchard.com>
2013-10-30 14:51:22 +0100

    Re-added MOSSCO - for the time being

---
+++ src/drivers/mossco/fabm_driver.F90
@@ -0,0 +1,26 @@
+#include "fabm_driver.h"
+
+module fabm_driver
+
+   implicit none
+
+   public fatal_error,log_message
+
+   private
+
+contains
+
+   subroutine fatal_error(routine,errormsg)
+     character(len=*), intent(in) :: routine,errormsg
+
+     FATAL trim(routine)//': '//trim(errormsg)
+     stop 1
+   end subroutine fatal_error
+
+   subroutine log_message(msg)
+     character(len=*), intent(in) :: msg
+
+     write (*,*) trim(msg)
+   end subroutine log_message
+
+end module fabm_driver
