fcd32715c51537023dcd3d0005148548881e809f
Jorn Bruggeman <jorn@bolding-burchard.com>
2014-06-12 12:26:14 +0100

    modular npzd: fixed module names; added shared module to 0d/gotm vs2010

--- src/models/examples/npzd/examples_npzd.F90
+++ src/models/examples/npzd/examples_npzd.F90
@@ -1,6 +1,6 @@
 module examples_npzd
-   use  fabm_examples_npzd_det
-   use  fabm_examples_npzd_nut
-   use  fabm_examples_npzd_phy
-   use  fabm_examples_npzd_zoo
+   use  examples_npzd_det
+   use  examples_npzd_nut
+   use  examples_npzd_phy
+   use  examples_npzd_zoo
 end module examples_npzd
