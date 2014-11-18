fb491ef39076b10afc0b99ee303de4f47509c462
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2014-11-18 15:57:38 +0000

    0d driver: moved driver-specific preprocessor macros to fabm_0d.h

---
+++ src/drivers/0d/fabm_0d.h
@@ -0,0 +1,17 @@
+! Taken from the General Ocean Turbulence Model:
+
+#define PATH_MAX	255
+
+#define stderr		0
+#define stdout		6
+
+#define STDOUT write(stdout,*)
+#define STDERR write(stderr,*)
+#define LEVEL0 STDERR
+#define LEVEL1 STDERR '   ',
+#define LEVEL2 STDERR '       ',
+#define LEVEL3 STDERR '           ',
+#define LEVEL4 STDERR '               ',
+#define FATAL  STDERR 'FATAL ERROR: ',
+
+#define LINE "------------------------------------------------------------------------"
--- src/drivers/0d/fabm_driver.h
+++ src/drivers/0d/fabm_driver.h
@@ -5,20 +5,3 @@
 ! because these are used in fabm.h.
 #include "fabm.h"
 
-! Taken from the General Ocean Turbulence Model:
-
-#define PATH_MAX	255
-
-#define stderr		0
-#define stdout		6
-
-#define STDOUT write(stdout,*)
-#define STDERR write(stderr,*)
-#define LEVEL0 STDERR
-#define LEVEL1 STDERR '   ',
-#define LEVEL2 STDERR '       ',
-#define LEVEL3 STDERR '           ',
-#define LEVEL4 STDERR '               ',
-#define FATAL  STDERR 'FATAL ERROR: ',
-
-#define LINE "------------------------------------------------------------------------"
