9e805207797c00bbdb0b8bc96d00daa3e5b14233
Jorn Bruggeman <jbr@pml.ac.uk>
2015-02-11 15:06:33 +0000

    fabm_complete_yaml.py: allow use from other modules, use values as interpreted by FABM

--- testcases/python/fabm_complete_yaml.py
+++ testcases/python/fabm_complete_yaml.py
@@ -2,12 +2,6 @@
 
 import sys
 
-if len(sys.argv)!=3:
-   print 'This script takes two argument: the path to the input YAML file and the path to the output YAML file.'
-   sys.exit(2)
-infile = sys.argv[1]
-outfile = sys.argv[2]
-
 try:
    import pyfabm
 except ImportError:
@@ -30,83 +24,92 @@ except ImportError:
     pass
 # ------------------------------------------
 
-# Create model object from YAML file.
-model = pyfabm.Model(infile)
+def processFile(infile,outfile):
+   # Create model object from YAML file.
+   model = pyfabm.Model(infile)
 
-# Load the old configuration
-with open(infile,'rU') as f:
-   config = yaml.load(f)
+   # Load the old configuration
+   with open(infile,'rU') as f:
+      config = yaml.load(f)
 
-def findMaximumDepth(d):
-   n = 0
-   for key,value in d.iteritems():
-      if isinstance(value,dict):
-         l = 2+findMaximumDepth(value)
-      else:
-         l = len(key)+2+len(str(value)) # key, folowed by ": ", followed by value
-      n = max(n,l)
-   return n
-
-def reorderParameters(modelname,parameters):
-   newparameters = collections.OrderedDict()
-   parameters_lower = dict([(key.lower(),value) for key,value in parameters.iteritems()])
-   modelname = modelname.lower()
-   for parameter in model.parameters:
-      if parameter.name.lower().startswith(modelname+'/'):
-         name = parameter.name[len(modelname)+1:].lower()
-         if name in parameters_lower: newparameters[name] = parameters_lower[name]
-   assert len(newparameters)>=len(parameters)
-   return newparameters
-
-def python2yaml(value):
-   if isinstance(value,bool):
-      return 'true' if value else 'false'
-   return str(value)
-
-def processDict(f,d,path=[]):
-   # If processing parameter list, reorder according to their registration by the model.
-   if len(path)==3 and path[-1]=='parameters':
-      d = reorderParameters(path[1],d)
-
-   # If processing a model dictionary, reorder according to prescribed order.
-   if len(path)==2 and path[0]=='instances':
-      newd = collections.OrderedDict()
-      order = ('long_name','model','parameters','initialization','coupling')
-      for key in order:
-         if key in d: newd[key] = d.pop(key)
-      assert not d,'Model "%s" contains unknown keys %s' % (path[1],', '.join(d.keys()))
-      d = newd
-
-   nspace = len(path)*2
-   for key,value in d.iteritems():
-      f.write(' '*nspace)
-      if isinstance(value,dict):
-         f.write('%s:\n' % key)
-         processDict(f,value,path=path+[key])
-      else:
-         value = python2yaml(value)
-         metadata = None
-         if len(path)==3:
-            if path[-1]=='parameters':
-               metadata = model.findParameter(path[1]+'/'+key,case_insensitive=True)
-            elif path[-1]=='initialization':
-               metadata = model.findStateVariable(path[1]+'_'+key)
-            elif path[-1]=='coupling':
-               try:
-                  metadata = model.findCoupling(path[1]+'/'+key)
-               except KeyError:
-                  pass
-         if metadata is not None:
-            f.write('%s: %s' % (metadata.name[len(path[1])+1:],value))
-            l = nspace+len(key)+2+len(str(value))
-            f.write('%s# %s' % (' '*(icommentstart-l),metadata.long_name,))
-            if metadata.units: f.write(' (%s)' % (metadata.units,))
-            if getattr(metadata,'default',None) is not None: f.write(', default = %s' % (python2yaml(metadata.default),))
+   def findMaximumDepth(d):
+      n = 0
+      for key,value in d.iteritems():
+         if isinstance(value,dict):
+            l = 2+findMaximumDepth(value)
          else:
-            f.write('%s: %s' % (key,value))
-         f.write('\n')
+            l = len(key)+2+len(str(value)) # key, folowed by ": ", followed by value
+         n = max(n,l)
+      return n
 
-icommentstart = findMaximumDepth(config)+3
-with open(outfile,'w') as f:   
-   processDict(f,config)
-      
+   def reorderParameters(modelname,parameters):
+      newparameters = collections.OrderedDict()
+      parameters_lower = dict([(key.lower(),value) for key,value in parameters.iteritems()])
+      modelname = modelname.lower()
+      for parameter in model.parameters:
+         if parameter.name.lower().startswith(modelname+'/'):
+            name = parameter.name[len(modelname)+1:].lower()
+            if name in parameters_lower: newparameters[name] = parameters_lower[name]
+      assert len(newparameters)>=len(parameters)
+      return newparameters
+
+   def python2yaml(value):
+      if isinstance(value,bool):
+         return 'true' if value else 'false'
+      return str(value)
+
+   def processDict(f,d,path=[]):
+      # If processing parameter list, reorder according to their registration by the model.
+      if len(path)==3 and path[-1]=='parameters':
+         d = reorderParameters(path[1],d)
+
+      # If processing a model dictionary, reorder according to prescribed order.
+      if len(path)==2 and path[0]=='instances':
+         newd = collections.OrderedDict()
+         order = ('long_name','model','parameters','initialization','coupling')
+         for key in order:
+            if key in d: newd[key] = d.pop(key)
+         assert not d,'Model "%s" contains unknown keys %s' % (path[1],', '.join(d.keys()))
+         d = newd
+
+      nspace = len(path)*2
+      for key,value in d.iteritems():
+         f.write(' '*nspace)
+         if isinstance(value,dict):
+            f.write('%s:\n' % key)
+            processDict(f,value,path=path+[key])
+         else:
+            metadata = None
+            if len(path)==3:
+               if path[-1]=='parameters':
+                  metadata = model.findParameter(path[1]+'/'+key,case_insensitive=True)
+               elif path[-1]=='initialization':
+                  metadata = model.findStateVariable(path[1]+'_'+key)
+               elif path[-1]=='coupling':
+                  try:
+                     metadata = model.findCoupling(path[1]+'/'+key)
+                  except KeyError:
+                     pass
+               if path[-1]!='coupling': value = metadata.value
+            value = python2yaml(value)
+            if metadata is not None:
+               f.write('%s: %s' % (metadata.name[len(path[1])+1:],value))
+               l = nspace+len(key)+2+len(str(value))
+               f.write('%s# %s' % (' '*(icommentstart-l),metadata.long_name,))
+               if metadata.units: f.write(' (%s)' % (metadata.units,))
+               if getattr(metadata,'default',None) is not None: f.write(', default = %s' % (python2yaml(metadata.default),))
+            else:
+               f.write('%s: %s' % (key,value))
+            f.write('\n')
+
+   icommentstart = findMaximumDepth(config)+3
+   with open(outfile,'w') as f:   
+      processDict(f,config)
+
+if __name__=='__main__':
+   if len(sys.argv)!=3:
+      print 'This script takes two argument: the path to the input YAML file and the path to the output YAML file.'
+      sys.exit(2)
+   infile = sys.argv[1]
+   outfile = sys.argv[2]
+   processFile(infile,outfile)
