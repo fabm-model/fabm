f640887d580acc48f718c1a121f899bb38726977
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2015-04-20 13:21:22 +0200

    added brom driver

---
+++ src/drivers/brom/fabm_driver.h
@@ -0,0 +1,11 @@
+#define _FABM_DIMENSION_COUNT_ 1
+#define _FABM_DEPTH_DIMENSION_INDEX_ 1
+#define _FABM_VECTORIZED_DIMENSION_INDEX_ 1
+
+!#define _FABM_VERTICAL_BOTTOM_TO_SURFACE_
+
+! Include FABM preprocessor definitions.
+! This *must* be done after the host-specific variables are defined (above),
+! because these are used in fabm.h.
+#include "fabm.h"
+
