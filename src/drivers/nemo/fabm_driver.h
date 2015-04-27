b13b1c92265a00fb405daa9fe8526a50f04ed435
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2015-04-27 16:29:40 +0100

    use double precision for nemo mask

--- src/drivers/nemo/fabm_driver.h
+++ src/drivers/nemo/fabm_driver.h
@@ -2,7 +2,7 @@
 #define _FABM_DEPTH_DIMENSION_INDEX_ 3
 #define _FABM_VECTORIZED_DIMENSION_INDEX_ 1
 
-#define _FABM_MASK_TYPE_ real
+#define _FABM_MASK_TYPE_ real(rk)
 #define _FABM_IS_UNMASKED_(maskvalue) maskvalue<=0
 
 #include "fabm.h"
