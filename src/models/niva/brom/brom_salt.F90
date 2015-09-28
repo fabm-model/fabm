f87ca2ae4afaed4ab5c39686a5a7bf6a4f93b34c
Jorn Bruggeman <jbr@pml.ac.uk>
2015-09-28 11:49:25 +0100

    expanded niva/brom_salt functionality

--- src/models/niva/brom/brom_salt.F90
+++ src/models/niva/brom/brom_salt.F90
@@ -23,17 +23,31 @@
    type,extends(type_base_model),public :: type_niva_brom_salt
 !     Variable identifiers
       type (type_state_variable_id)        :: id_Na,id_Cl,id_NaCl
+      type (type_bottom_state_variable_id) :: id_NaCl_bot
       type (type_dependency_id)            :: id_temp
             
- real(rk) :: K_NaCl != 37.19e12 ! NaCl conditional equilibrium constant %  37.19 mol2/l2    (Krumgalz, Millero, 1989 )   !!1.8e-11 (M) (Internet)            
- real(rk) :: K_NaCl_diss !=2. ! Specific rate of dissolution of NaCl kg/m3/d !!!(1/day)   !2.5 X 10-1 yr-1 (vanCap06)
- real(rk) :: K_NaCl_form !=1. ! Specific rate of formation of NaCl kg/m3/d !!!(1/day)  !! 1. X 10-4 yr-1(vanCap06)    
- real(rk) :: Wsed != 5. !1Rate of sinking of detritus (POP, PON)d-1 !!  Wdetr=1.5 (Savchuk, Wulff,1996),!Wdetr= 3.5; 20. (Gregoire,2000)
+      type (type_diagnostic_variable_id)            :: id_psu
+      type (type_diagnostic_variable_id)            :: id_NaCl_sat
+      type (type_horizontal_diagnostic_variable_id) :: id_h_bot
+
+      real(rk) :: K_NaCl
+      real(rk) :: K_NaCl_diss
+      real(rk) :: K_NaCl_form
+      real(rk) :: w
+      real(rk) :: porosity
 
    contains
       procedure :: initialize
       procedure :: do
+      procedure :: do_bottom
    end type
+
+   real(rk), parameter :: u_Na = 22.9898_rk ! atomic masss of sodium in g/mol
+   real(rk), parameter :: u_Cl = 35.453_rk  ! atomic masss of chloride in g/mol
+   real(rk), parameter :: NaCl_density = 2.16e6_rk ! 2.16 g/cm3 = 2.16e6 g/m3
+
+   type (type_bulk_standard_variable),parameter :: total_sodium   = type_bulk_standard_variable(name='total_sodium',  units='mmol/m**3', aggregate_variable=.true., conserved=.true.)
+   type (type_bulk_standard_variable),parameter :: total_chloride = type_bulk_standard_variable(name='total_chloride',units='mmol/m**3', aggregate_variable=.true., conserved=.true.)
 !EOP
 !-----------------------------------------------------------------------
 
@@ -60,25 +74,30 @@
 !EOP
 !-----------------------------------------------------------------------
 !BOC
-   call self%register_state_variable(self%id_Na, 'Na', 'mmol/m**3','sodium', minimum=0.0_rk)
-   call self%register_state_variable(self%id_Cl, 'Cl', 'mmol/m**3','chloride', minimum=0.0_rk)
-   call self%register_state_variable(self%id_NaCl, 'NaCl', 'mmol/m**3','sodium chloride', minimum=0.0_rk,vertical_movement=-self%Wsed/86400._rk)
+   call self%get_parameter(self%K_NaCl,     'K_NaCl',     'mmol**2/m**6','NaCl conditional equilibrium constant',default=37.19e12_rk)
+   call self%get_parameter(self%K_NaCl_diss,'k_NaCl_diss','mmol/m**3/d', 'Specific rate of dissolution of NaCl',default=1._rk,scale_factor=1._rk/86400._rk)
+   call self%get_parameter(self%K_NaCl_form,'k_NaCl_form','mmol/m**3/d', 'Specific rate of formation of NaCl',default=1._rk,scale_factor=1._rk/86400._rk)
+   call self%get_parameter(self%w,          'w',          'm/d',         'Sedimentation rate for particulate NaCl',default=1._rk,scale_factor=1._rk/86400._rk)
+   call self%get_parameter(self%porosity,   'porosity',   '-',           'Porosity of precipitated NaCl',default=0.5_rk,scale_factor=1._rk/86400._rk)
 
-   call self%get_parameter(self%K_NaCl,'K_NaCl','umol2/l2',' NaCl conditional equilibrium constant',default=37.19e12_rk)
-   call self%get_parameter(self%K_NaCl_diss,'K_NaCl_diss','umol/l/d',' Specific rate of dissolution of NaCl',default=1._rk)
-   call self%get_parameter(self%K_NaCl_form,'K_NaCl_form','umol/l/d',' Specific rate of formation of NaCl',default=1._rk)
-   call self%get_parameter(self%K_NaCl_form,'Wsed','m/d',' Specific rate of formation of NaCl',default=1._rk)
-   
- !  real(rk) :: K_NaCl != 37.19e12 ! NaCl conditional equilibrium constant %  37.19 mol2/l2    (Krumgalz, Millero, 1989 )   !!1.8e-11 (M) (Internet)            
- !real(rk) :: K_NaCl_diss !=2. ! Specific rate of dissolution of NaCl kg/m3/d !!!(1/day)   !2.5 X 10-1 yr-1 (vanCap06)
- !real(rk) :: K_NaCl_form !=1. ! Specific rate of formation of NaCl kg/m3/d !!!(1/day)  !! 1. X 10-4 yr-1(vanCap06)    
- !real(rk) :: Wsed != 5. !1Rate of sinking of detritus (POP, PON)d-1 !!  Wdetr=1.5 (Savchuk, Wulff,1996),!Wdetr= 3.5; 20. (Gregoire,2000)
+   call self%register_state_variable(self%id_Na,   'Na',   'mmol/m**3','sodium',   minimum=0.0_rk)
+   call self%register_state_variable(self%id_Cl,   'Cl',   'mmol/m**3','chloride', minimum=0.0_rk)
+   call self%register_state_variable(self%id_NaCl, 'NaCl', 'mmol/m**3','particulate sodium chloride', minimum=0.0_rk,vertical_movement=-self%w)
 
-   
-   
+   call self%register_state_variable(self%id_NaCl_bot, 'NaCl_bot', 'mmol/m**2','particulate sodium chloride at bottom', minimum=0.0_rk)
+
+   call self%register_diagnostic_variable(self%id_psu,     'salt' ,   '-','salinity in PSU', standard_variable=standard_variables%practical_salinity)
+   call self%register_diagnostic_variable(self%id_NaCl_sat,'NaCl_sat','-','sodium chloride saturation')
+   call self%register_diagnostic_variable(self%id_h_bot,   'h_bot',   'm','thickness of bottom salt layer')
+
+   call self%add_to_aggregate_variable(total_sodium, self%id_Na)
+   call self%add_to_aggregate_variable(total_sodium, self%id_NaCl)
+   call self%add_to_aggregate_variable(total_sodium, self%id_NaCl_bot)
+   call self%add_to_aggregate_variable(total_chloride, self%id_Cl)
+   call self%add_to_aggregate_variable(total_chloride, self%id_NaCl)
+   call self%add_to_aggregate_variable(total_chloride, self%id_NaCl_bot)
+
    call self%register_dependency(self%id_temp,standard_variables%temperature)
-   
-    self%dt = 86400.
 
    end subroutine initialize
 !EOC
@@ -111,29 +130,74 @@
    _LOOP_BEGIN_
 
    ! Environment
-   _GET_(self%id_temp,temp)              ! temperature
+   _GET_(self%id_temp,temp)              ! temperature - not used yet
 
    _GET_(self%id_Na,Na)
    _GET_(self%id_Cl,Cl)
    _GET_(self%id_NaCl,NaCl)
-   
-   
-! NaCl precipitation/dissolution
-        Om_NaCl=Na*Cl/(self%K_NaCl)
-!% Ni2+ + 2HCO3- = MnCO3 +CO2 +H2O (vanCap)
-        NaCl_prec=self%K_NaCl_form*max(0._rk,(Om_NaCl-1._rk))
-!% 
-        NaCl_diss=self%K_NaCl_diss*max(0._rk,(1._rk-Om_NaCl))   
-   
-   
-   _SET_ODE_(self%id_Na,-NaCl_prec+NaCl_diss)
-   _SET_ODE_(self%id_Cl,-NaCl_prec+NaCl_diss)
+
+   ! NaCl saturation state (NB all units mmol/m**3)
+   Om_NaCl=Na*Cl/(self%K_NaCl)
+   _SET_DIAGNOSTIC_(self%id_NaCl_sat,Om_NaCl)
+
+   ! Precipitation/dissolution
+   NaCl_prec=self%K_NaCl_form*max(0._rk,(Om_NaCl-1._rk))
+   NaCl_diss=self%K_NaCl_diss*max(0._rk,(1._rk-Om_NaCl))
+
+   _SET_ODE_(self%id_Na,  -NaCl_prec+NaCl_diss)
+   _SET_ODE_(self%id_Cl,  -NaCl_prec+NaCl_diss)
    _SET_ODE_(self%id_NaCl,+NaCl_prec-NaCl_diss)
-  
-! Leave spatial loops (if any)
+
+   ! From mmol/m3 to g/L (note: if we want g/kg, we'd need to divide by density rather than 1000)
+   _SET_DIAGNOSTIC_(self%id_psu,(Na*u_Na+Cl*u_Cl)*1e-6_rk)
+
+   ! Leave spatial loops (if any)
    _LOOP_END_
 
    end subroutine do
+!EOC
+
+
+!-----------------------------------------------------------------------
+!BOP
+!
+! !IROUTINE: 
+!
+! !INTERFACE:
+   subroutine do_bottom(self,_ARGUMENTS_DO_BOTTOM_)
+!
+! !DESCRIPTION:
+! 
+!
+! !INPUT PARAMETERS:
+   class (type_niva_brom_salt),intent(in) :: self
+   _DECLARE_ARGUMENTS_DO_BOTTOM_
+!
+! !REVISION HISTORY:
+!  Original author(s): 
+!
+! !LOCAL VARIABLES:
+   real(rk) :: NaCl,NaCl_bot
+!EOP
+!-----------------------------------------------------------------------
+!BOC
+   ! Enter spatial loops (if any)
+   _HORIZONTAL_LOOP_BEGIN_
+
+   ! Environment
+   _GET_(self%id_NaCl,NaCl)
+   _GET_HORIZONTAL_(self%id_NaCl_bot,NaCl_bot)
+
+   _SET_BOTTOM_EXCHANGE_(self%id_NaCl,-self%w*NaCl)
+   _SET_BOTTOM_ODE_(self%id_NaCl_bot, +self%w*NaCl)
+
+   ! From mmol/m2 to thickness: first to g/m2 (multiply by molecuar weight), then to m by dividing by density in g/m3, accounting for porosity (void fraction)
+   _SET_HORIZONTAL_DIAGNOSTIC_(self%id_h_bot,NaCl_bot*(u_Na+u_Cl)/1000/NaCl_density/(1-self%porosity))
+
+   ! Leave spatial loops (if any)
+   _HORIZONTAL_LOOP_END_
+
+   end subroutine do_bottom
 !EOC
 
 end module
