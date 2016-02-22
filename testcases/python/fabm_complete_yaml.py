911cd5a04985375757fac642c54902d8e4063d88
Jorn Bruggeman <jbr@pml.ac.uk>
2016-02-22 14:04:33 +0000

    fabm_complete_yaml.py: by default update input file in-place

--- testcases/python/fabm_complete_yaml.py
+++ testcases/python/fabm_complete_yaml.py
@@ -8,9 +8,10 @@ except ImportError:
    print 'Unable to load pyfabm. Please build and install python-fabm by running cmake+make for source directory $FABMDIR/src/drivers/python.'
    sys.exit(1)
 
-if len(sys.argv)!=3:
-   print 'This script takes two argument: the path to the input YAML file and the path to the output YAML file.'
+if len(sys.argv)<2:
+   print 'This script takes at least one argument: the path to the YAML file that needs to be completed. Optionally, the path to the output file can be given as second argument. By default, the input file is modified in-place.'
    sys.exit(2)
 infile = sys.argv[1]
-outfile = sys.argv[2]
+outfile = infile
+if len(sys.argv)>2: outfile = sys.argv[2]
 pyfabm.complete_yaml.processFile(infile,outfile)
