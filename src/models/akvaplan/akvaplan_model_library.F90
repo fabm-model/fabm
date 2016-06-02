2d0b4fafba968119bb846deb1a9a211de56189db
Jorn Bruggeman <jbr@pml.ac.uk>
2016-06-02 15:56:35 +0100

    added akvaplan/tracer model

---
+++ src/models/akvaplan/akvaplan_model_library.F90
@@ -0,0 +1,36 @@
+module akvaplan_model_library
+
+   ! Library with biogeochemical models developed by Akvaplan-niva (http://www.akvaplan.niva.no).
+   ! Copyright (C) 2016 - Akvaplan-niva
+
+   use fabm_types, only: type_base_model_factory,type_base_model
+
+   use akvaplan_tracer
+
+   implicit none
+
+   private
+
+   type,extends(type_base_model_factory) :: type_factory
+      contains
+      procedure :: create
+   end type
+
+   type (type_factory),save,target,public :: akvaplan_model_factory
+
+contains
+
+   subroutine create(self,name,model)
+      class (type_factory),intent(in) :: self
+      character(*),        intent(in) :: name
+      class (type_base_model),pointer :: model
+
+      select case (name)
+         case ('tracer'); allocate(type_tracer::model)
+         ! Add new models here
+         case default
+            call self%type_base_model_factory%create(name,model)
+      end select
+   end subroutine create
+
+end module
