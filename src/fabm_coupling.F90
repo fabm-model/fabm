3533c1a8a6b0f75e60feebadc8164589b76f40c5
Jorn Bruggeman <jbr@pml.ac.uk>
2016-08-24 14:37:17 +0100

    simplified create_conservation_checks

--- src/fabm_coupling.F90
+++ src/fabm_coupling.F90
@@ -701,8 +701,7 @@ recursive subroutine create_conservation_checks(self)
             ! Enumerate contributions to aggregate variable.
             contributing_variable => aggregate_variable%first_contributing_variable
             do while (associated(contributing_variable))
-               if (.not.contributing_variable%link%original%state_indices%is_empty().and.index(contributing_variable%link%name,'/')==0) then
-                  ! Contributing variable was originally registered as a state variable (or state dependency)
+               if (index(contributing_variable%link%name,'/')==0) then
                   select case (contributing_variable%link%original%domain)
                      case (domain_interior)
                         if (associated(contributing_variable%link%original%sms))          call sum%add_component        (contributing_variable%link%original%sms%name,         contributing_variable%scale_factor)
