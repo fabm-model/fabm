cd19eceda1310c56220a62649fb3ca60d67931df
Jorn Bruggeman <jorn@bolding-burchard.com>
2017-01-17 22:04:06 +0000

    add diagnostics to outputs

--- src/drivers/particles/particle_driver.F90
+++ src/drivers/particles/particle_driver.F90
@@ -2,7 +2,7 @@ module fabm_particle_driver
 
    use fabm
    use fabm_config
-   use fabm_types, only: rk, attribute_length
+   use fabm_types, only: rk, attribute_length, output_none
    use fabm_driver, only: driver
 
    implicit none
@@ -18,6 +18,7 @@ module fabm_particle_driver
       real(rk), pointer               :: data(:)   => null()
       logical                         :: save      = .false.
       type (type_output), pointer     :: specific_to => null()
+      integer                         :: id        = -1
       type (type_output), pointer     :: next      => null()
    end type
 
@@ -84,14 +85,33 @@ module fabm_particle_driver
       allocate(self%work(self%npar, self%nstate))
 
       particle_count_output => null()
-      do ivar=1,size(self%model%state_variables)
+      do ivar = 1, size(self%model%state_variables)
+         output => add_output(self%model%state_variables(ivar), .true.)
+         output%data => self%y(:, ivar)
+      end do
+      do ivar = 1, size(self%model%diagnostic_variables)
+         output => add_output(self%model%diagnostic_variables(ivar), self%model%diagnostic_variables(ivar)%output /= output_none)
+         output%id = ivar
+      end do
+
+      allocate(self%active(self%npar))
+      self%active = .true.
+      self%active_hz = .false.
+
+   contains
+
+      function add_output(variable, save) result(output)
+         class (type_external_variable), intent(in) :: variable
+         logical,                        intent(in) :: save
+
+         type (type_output), pointer :: output
+
          allocate(output)
-         output%name      = self%model%state_variables(ivar)%name
-         output%units     = self%model%state_variables(ivar)%units
-         output%long_name = self%model%state_variables(ivar)%long_name
-         output%save      = .true.
-         output%data      => self%y(:, ivar)
-         if (self%model%state_variables(ivar)%target%specific_to == -2) then
+         output%name      = variable%name
+         output%units     = variable%units
+         output%long_name = variable%long_name
+         output%save      = save
+         if (variable%target%specific_to == -2) then
             ! This is a variable that describes a property specific to water parcels (e.g., age).
             ! To average this over a volume, we need to sum its value over all particles in the volume,
             ! then divide by the number of particles in the volume. The averging infrastucture
@@ -112,7 +132,7 @@ module fabm_particle_driver
                self%first_output => particle_count_output
             end if
             output%specific_to => particle_count_output
-         elseif (self%model%state_variables(ivar)%target%specific_to /= -1) then
+         elseif (variable%target%specific_to /= -1) then
             ! This is a variable that describes a property specific to one of the model's other (non-property) state variables.
             ! To average this over a volume, we need to compute \Sigma c w / \Sigma w,
             ! with c being the property value, w being the variable that the property pertains to, and sum \Sigma taken over all particles in the volume.
@@ -122,16 +142,22 @@ module fabm_particle_driver
          ! Prepend to list of outputs
          output%next => self%first_output
          self%first_output => output
-      end do
+      end function add_output
 
-      allocate(self%active(self%npar))
-      self%active = .true.
-      self%active_hz = .false.
    end subroutine configure
 
    subroutine initialize(self)
 !DEC$ ATTRIBUTES DLLEXPORT :: initialize
       class (type_fabm_particle_state), intent(inout) :: self
+
+      type (type_output), pointer :: output
+
+      ! Copy "save" attribute of particle property to FABM diagnostics
+      output => self%first_output
+      do while (associated(output))
+         if (output%id /= -1) self%model%diagnostic_variables(output%id)%save = output%save
+         output => output%next
+      end do
 
       ! Domain size: number of particles
       call fabm_set_domain(self%model, self%npar)
@@ -145,6 +171,13 @@ module fabm_particle_driver
 
       ! Provide pointers to the particle state.
       call self%model%link_all_interior_state_data(self%y)
+
+      ! Fo saved diagnostics, obtain a pointer to their values.
+      output => self%first_output
+      do while (associated(output))
+         if (output%id /= -1 .and. output%save) output%data => fabm_get_interior_diagnostic_data(self%model, output%id)
+         output => output%next
+      end do
    end subroutine initialize
 
    subroutine start(self, domain_per_particle)
