fa2292f038aaf762d2f9a786f060fa47194ca589
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2017-01-18 15:47:05 +0000

    support overwriting of default value for running mean

--- src/fabm_expressions.F90
+++ src/fabm_expressions.F90
@@ -133,9 +133,10 @@ contains
       if (present(maximum_depth)) expression%maximum_depth = maximum_depth
    end function
 
-   function bulk_temporal_mean(input,period,resolution) result(expression)
+   function bulk_temporal_mean(input,period,resolution,missing_value) result(expression)
       type (type_dependency_id), intent(inout),target   :: input
       real(rk),                  intent(in)             :: period,resolution
+      real(rk),optional,         intent(in)             :: missing_value
 
       type (type_bulk_temporal_mean) :: expression
       character(len=attribute_length) :: prefix,postfix
@@ -151,6 +152,7 @@ contains
       expression%in => input%index
       expression%n = nint(period/resolution)
       expression%period = period
+      if (present(missing_value)) expression%missing_value = missing_value
    end function
 
    function horizontal_temporal_mean(input,period,resolution) result(expression)
