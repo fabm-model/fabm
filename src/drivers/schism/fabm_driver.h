0486733d79736cd74588ac6f766c3cdf1781b399
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2017-09-15 16:38:22 +0100

    added schism driver - Hofmeister

---
+++ src/drivers/schism/fabm_driver.h
@@ -0,0 +1,13 @@
+! Copyright 2017 Helmholtz-Zentrum Geesthacht
+! Licensed under the Apache License, Version 2.0
+!  (http://www.apache.org/licenses/LICENSE-2.0)
+! Author(s): Richard Hofmeister
+
+#define _FABM_DIMENSION_COUNT_ 2
+#define _FABM_DEPTH_DIMENSION_INDEX_ 1
+#define _FABM_VECTORIZED_DIMENSION_INDEX_ 1
+
+#define _FABM_VERTICAL_BOTTOM_TO_SURFACE_
+#define _FABM_BOTTOM_INDEX_ -1
+
+#include "fabm.h"
