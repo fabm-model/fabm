6d2098c547d5edfadd53c1775e8a3cb86398fa69
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2018-11-22 21:36:57 +0000

    write unit test output to log files

--- scripts/developers/run_unit_tests.py
+++ scripts/developers/run_unit_tests.py
@@ -5,29 +5,65 @@ import os.path
 import tempfile
 import subprocess
 import shutil
+import argparse
 
 script_root = os.path.abspath(os.path.dirname(__file__))
+root = os.path.join(script_root, '../..')
+allowed_hosts = os.listdir(os.path.join(root, 'src/drivers'))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--host', action='append', dest='hosts', choices=allowed_hosts)
+args = parser.parse_args()
+
 cmake_arguments = []
 
 generates = {}
 builds = {}
 tests = {}
 
-root = os.path.join(script_root, '../..')
-hosts = os.listdir(os.path.join(root, 'src/drivers'))
+if not args.hosts:
+    args.hosts = allowed_hosts
+print('Selected hosts: %s' % ', '.join(args.hosts))
+
+nfailed = 0
+logs = []
+def run(phase, args, **kwargs):
+    global nfailed
+    proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, **kwargs)
+    stdoutdata, stderrdata = proc.communicate()
+    print('SUCCESS' if proc.returncode == 0 else 'FAILED')
+    if proc.returncode != 0:
+        log_path = '%s.log' % phase
+        with open(log_path, 'w') as f:
+            f.write(stdoutdata)
+        nfailed += 1
+        logs.append(log_path)
+    return proc.returncode
 
 build_root = tempfile.mkdtemp()
 try:
-    for host in hosts:
-        builddir = os.path.join(build_root, host)
-        os.mkdir(builddir)
-        generates[host] = subprocess.call(['cmake', os.path.join(root, 'src'), '-DFABM_HOST=%s' % host] + cmake_arguments, cwd=builddir)
+    for host in args.hosts:
+        print(host)
+        build_dir = os.path.join(build_root, host)
+        os.mkdir(build_dir)
+        print('  generating...', end='')
+        generates[host] = run('%s_generate' % host, ['cmake', os.path.join(root, 'src'), '-DFABM_HOST=%s' % host] + cmake_arguments, cwd=build_dir)
         if generates[host] != 0:
             continue
-        builds[host] = subprocess.call(['cmake', '--build', builddir, '--target', 'test_host'])
+        print('  building...', end='')
+        builds[host] = run('%s_build' % host, ['cmake', '--build', build_dir, '--target', 'test_host'])
         if builds[host] != 0:
             continue
-        if os.path.isfile(os.path.join(builddir, 'Debug/test_host.exe')):
-            tests[host] = subprocess.call([os.path.join(builddir, 'Debug/test_host.exe')])
+        print('  testing...', end='')
+        if os.path.isfile(os.path.join(build_dir, 'Debug/test_host.exe')):
+            tests[host] = run('%s_test' % host, [os.path.join(build_dir, 'Debug/test_host.exe')])
+        else:
+            tests[host] = run('%s_test' % host, [os.path.join(build_dir, 'test_host')])
 finally:
     shutil.rmtree(build_root)
+
+if nfailed == 0:
+    print('All tests complete - no failures')
+else:
+    print('All tests complete - %i FAILED' % nfailed)
+    print('See the following log files:\n%s' % '\n'.join(logs))
