c1b6f763badb87eb511b50c3e44da01f7924b08f
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2020-04-03 13:12:58 +0100

    fixed missing dependency reporting

--- src/fabm_graph.F90
+++ src/fabm_graph.F90
@@ -124,9 +124,9 @@ module fabm_graph
 
 contains
 
-function output_variable_set_add(self,variable) result(output_variable)
-   class (type_output_variable_set),intent(inout) :: self
-   type (type_internal_variable),target           :: variable
+function output_variable_set_add(self, variable) result(output_variable)
+   class (type_output_variable_set), intent(inout) :: self
+   type (type_internal_variable),target            :: variable
 
    type (type_output_variable_set_node), pointer :: node
    type (type_output_variable),          pointer :: output_variable
@@ -407,8 +407,7 @@ contains
       type (type_node),            pointer :: node
       type (type_output_variable), pointer :: output_variable
 
-      _ASSERT_(variable%source /= source_unknown, 'graph_add_variable::add_call', 'Variable "' // trim(variable%name) // '" was registered with source_unknown')
-      if (variable%source == source_constant .or. variable%source == source_state .or. variable%source == source_external) return
+      if (variable%source == source_constant .or. variable%source == source_state .or. variable%source == source_external .or. variable%source == source_unknown) return
       _ASSERT_ (.not. variable%write_indices%is_empty(), 'graph_add_variable::add_call', 'Variable "' // trim(variable%name) // '" with source ' // trim(source2string(variable%source)) // ' does not have a write index')
 
       node => self%add_call(variable%owner, variable%source, outer_calls)
