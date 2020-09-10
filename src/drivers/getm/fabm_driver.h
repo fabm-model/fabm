8ae9efe0901edbdbea3ad3b75d3e241f17e049e6
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2020-09-10 09:32:45 +0100

    removed getm driver placeholder (getm uses gotm driver)

--- src/drivers/getm/fabm_driver.h
+++
@@ -1,9 +0,0 @@
-#define _FABM_DIMENSION_COUNT_ 3
-#define _FABM_DEPTH_DIMENSION_INDEX_ 3
-#define _FABM_VECTORIZED_DIMENSION_INDEX_ 1
-
-#define _FABM_MASK_TYPE_ integer
-#define _FABM_UNMASKED_VALUE_ 1
-
-#include "fabm.h"
-
