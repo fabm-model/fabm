e2fe93268e96d590c3911ad3ae08703a9e63b4a7
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2021-02-17 16:40:22 +0000

    fix pyfabm library build

--- src/drivers/python/fabm_driver.h
+++ src/drivers/python/fabm_driver.h
@@ -1,8 +1,6 @@
 #ifndef _FABM_DIMENSION_COUNT_
-#  error "_FABM_DIMENSION_COUNT_ must be defined."
-#endif
-
-#if _FABM_DIMENSION_COUNT_==1
+#  define _FABM_DIMENSION_COUNT_ 0
+#elif _FABM_DIMENSION_COUNT_==1
 #  define _FABM_VECTORIZED_DIMENSION_INDEX_ 1
 #  define _FABM_CONTIGUOUS_
 #endif
