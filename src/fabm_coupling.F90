a9226fdd0217b7446a27e570ab6832f0e551d74f
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2021-07-08 18:59:01 +0100

    coupling: do generate placeholder standard variable if state dependency is optional

--- src/fabm_coupling.F90
+++ src/fabm_coupling.F90
@@ -235,7 +235,8 @@ contains
                      link => link%next
                   end do
 
-                  if (stage == couple_final .and. .not. associated(link) .and. coupling%slave%target%source /= source_state) then
+                  if (stage == couple_final .and. .not. associated(link) .and. (coupling%slave%target%source /= source_state &
+                     .or. coupling%slave%target%presence == presence_external_optional)) then
                      ! Target variable was not found, but this is our last chance.
                      ! Therefore, create a placeholder variable at the root level.
                      ! This variable will still need to be provided by the host.
