7a8025109d1ec4f6e794ff48d3c6782a6044f8df
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2023-06-30 10:58:37 +0100

    added FABM_EXTRA_INSTITUTES

--- util/developers/run_all_testcases.py
+++ util/developers/run_all_testcases.py
@@ -278,10 +278,7 @@ if __name__ == '__main__':
         cmake_arguments.append('-DFABM_%s_BASE=%s' % (name.upper(), basedir))
 
     if len(args.ext) > 0:
-        names = set([n for n in os.listdir(os.path.join(fabm_base, 'src/models')) if n not in ('hzg', 'metu', 'python')])
-        for e in args.ext:
-            names.add(e[0])
-        cmake_arguments.append('-DFABM_INSTITUTES=%s' % ';'.join(sorted(names)))
+        cmake_arguments.append('-DFABM_EXTRA_INSTITUTES=%s' % ';'.join([e[0] for e in args.ext]))
 
     args.cmake_arguments = cmake_arguments
 
