6916342cf5652f44146e6492b33cb437e89775ea
Karsten Bolding <karsten@bolding-bruggeman.com>
2024-03-13 15:15:53 +0100

    added initial support for UVic-ESCM (v2.10)

---
+++ src/drivers/uvic/fabm_driver.h
@@ -0,0 +1,12 @@
+#define _FABM_DIMENSION_COUNT_ 3
+#define _FABM_DEPTH_DIMENSION_INDEX_ 2
+#define _FABM_VECTORIZED_DIMENSION_INDEX_ 1
+
+#define _FABM_MASK_TYPE_ real(rke)
+#define _FABM_UNMASKED_VALUE_ 1
+
+! Specify that the vertical index of the bottom cell is variable (kmax depends on i,j)
+#define _FABM_BOTTOM_INDEX_ -1
+
+#include "fabm.h"
+
