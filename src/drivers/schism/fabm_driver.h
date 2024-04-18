e3f2cd2742d94c7c916f758f35e297504c0d275b
Carsten Lemmen <carsten.lemmen@hereon.de>
2024-04-18 13:38:05 +0200

    Update src/drivers/schism/fabm_driver.h with MASK definitions
    Recent versions of SCHISM need the MASK_TYPE and MASK_VALUE to be defined in the driver.  This Commit adds the relevant definitions.  We also update copyright and license (placed in public domain).

--- src/drivers/schism/fabm_driver.h
+++ src/drivers/schism/fabm_driver.h
@@ -1,7 +1,8 @@
-! Copyright 2017 Helmholtz-Zentrum Geesthacht
-! Licensed under the Apache License, Version 2.0
-!  (http://www.apache.org/licenses/LICENSE-2.0)
-! Author(s): Richard Hofmeister
+! SPDX-FileCopyrightText: 2021-2024 Helmholtz-Zentrum hereon GmbH
+! SPDX-FileCopyrightText: 2017-2021 Helmholtz-Zentrum Geesthacht GmbH
+! SPDX-License-Identifier: CC0-1.0
+! SPDX-FileContributor: Carsten Lemmen <carsten.lemmen@hereon.de>
+! SPDX-FileContributor: Richard Hofmeister
 
 #define _FABM_DIMENSION_COUNT_ 2
 #define _FABM_DEPTH_DIMENSION_INDEX_ 1
@@ -10,4 +11,8 @@
 #define _FABM_VERTICAL_BOTTOM_TO_SURFACE_
 #define _FABM_BOTTOM_INDEX_ -1
 
+#define _FABM_MASK_TYPE_ integer
+#define _FABM_MASKED_VALUE_ 1
+
 #include "fabm.h"
+
