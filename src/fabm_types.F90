54fc83791a66dc5505a4607002828aa2922f4804
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2025-07-17 19:13:28 +0100

    limit implements calls

--- src/fabm_types.F90
+++ src/fabm_types.F90
@@ -1111,7 +1111,9 @@ contains
       link => model%links%first
       do while (associated(link))
          if (index(link%name, '/') == 0) then
-            if (.not. model%implements(link%target%source)) link%target%source = source_constant
+            if (source /= source_unknown .or. source /= source_state) then
+               if (.not. model%implements(link%target%source)) link%target%source = source_constant
+            end if
          end if
          link => link%next
       end do
