07d76b5308eef23936a140ccefed51763b794232
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2026-09-23 15:44:43 +0100

    potential workaround for nvfortran/aocc bug

--- src/fabm_types.F90
+++ src/fabm_types.F90
@@ -1556,7 +1556,7 @@ contains
             'Parameterized coupling ' // trim(target_name) // ' should end with closing parenthesis.')
          call request_parameterized_coupling(target_name(1:istart-1), target_name(istart+1:istop-1))
       else
-         call request_coupling_lt(self, link, type_named_coupling_target(target_name), priority)
+         call request_coupling_lt(self, link, type_named_coupling_target(name=target_name), priority)
       end if
 
    contains
